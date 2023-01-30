f1efffa2fcc7676904db74f418f9c9b2efcfd66e
Krishnendra <krish.ksg@gmail.com>
2023-01-30 20:37:19 +0530

    class 16 updates

--- src/App.js
+++ src/App.js
@@ -6,18 +6,39 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Footer from "./components/Footer";
 import Textform from "./pages/Textform";
+import Alert from "./components/Alert";
 
 function App() {
   const [color, setColor] = useState("#FF0000");
-  const [mode, setMode] = useState("light");
+  const [mode, setMode] = useState("Light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message) => {
+    setAlert({
+      msg: message,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 5000);
+  };
+
+  const toggleMode = () => {
+    if (mode === "Light") {
+      setMode("Dark");
+    } else {
+      setMode("Light");
+    }
+  };
+
   return (
     <>
       <Router>
         <Navbar />
+        <Alert alert={alert} />
         <Routes>
           <Route path="/" element={<Home text="This is Home Page of React Course Demo App" c={color} setColor={setColor} />} />
           <Route path="/about" element={<About />} />
-          <Route path="/textform" element={<Textform mode="Dark" heading="Text Form Application" />} />
+          <Route path="/textform" element={<Textform mode={mode} toggleMode={toggleMode} showAlert={showAlert} heading="Text Form Application" />} />
         </Routes>
         <Footer />
       </Router>
--- src/pages/Textform.js
+++ src/pages/Textform.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 const Textform = (props) => {
   const [text, setText] = useState("");
+
   const handleTextareaOnChange = (event) => {
     setText(event.target.value);
   };
@@ -9,14 +10,24 @@ const Textform = (props) => {
   const handleOnclickUpper = () => {
     let newtext = text.toUpperCase();
     setText(newtext);
+    props.showAlert("Text converted to Upper Case!!");
   };
 
   const handleOnclickLower = () => {
     let newtext = text.toLowerCase();
     setText(newtext);
+    props.showAlert("Text converted to Lower Case!!");
   };
 
-  const handleCleartext = () => {};
+  const handleCleartext = () => {
+    setText("");
+    props.showAlert("Text Cleared!!");
+  };
+
+  const handleCopytext = () => {
+    navigator.clipboard.writeText(text);
+    props.showAlert("Text Copied Successfully!!");
+  };
 
   return (
     <>
@@ -25,6 +36,9 @@ const Textform = (props) => {
         <div className="mb-3">
           <textarea className="form-control" value={text} onChange={handleTextareaOnChange} rows="9" style={{ background: props.mode === "Dark" ? "black" : "white", color: props.mode === "Dark" ? "white" : "black" }} id="myBox"></textarea>
         </div>
+        <button className="btn btn-primary mx-1 my-1" onClick={props.toggleMode}>
+          Change Color Mode
+        </button>
         <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleOnclickUpper}>
           Convert to UpperCase
         </button>
@@ -33,6 +47,9 @@ const Textform = (props) => {
         </button>
         <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleCleartext}>
           Clear Text
+        </button>
+        <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleCopytext}>
+          Copy Text
         </button>
       </div>
 
